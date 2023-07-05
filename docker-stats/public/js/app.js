1820ce615d87cf5e73b994e0f49d850c4527240b
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-07-05 15:01:58 +0500

    [NEW] docker stats

---
+++ docker-stats/public/js/app.js
@@ -0,0 +1,35 @@
+var socket = io();
+socket.on('docker stats', function(containers){
+    containers.sort((a, b) => b.memUnit - a.memUnit || b.memValue - a.memValue);
+    var headersHtml = '';
+    if (containers.length > 0) {
+        for (var key in containers[0]) {
+            if (key != 'memValue' && key != 'memUnit') {
+                headersHtml += '<th>' + key + '</th>';
+            }
+        }
+    }
+    document.getElementById('thead').innerHTML = headersHtml;
+
+    var html = '';
+    for (var i=0; i < containers.length; i++){
+        var c = containers[i];
+        var memClass = '';
+        if (c.memValue > 2048 && c.memValue <= 5120) memClass = 'warning'; // 2 GB to 5 GB (in MiB)
+        else if (c.memValue > 5120) memClass = 'danger'; // over 5 GB (in MiB)
+        else memClass = 'safe'; // less than 2 GB (in MiB)
+
+        html += '<tr>';
+        for (var key in c) {
+            if (key != 'memValue' && key != 'memUnit') {
+                if (key === "mem usage / limit") {
+                    html += '<td class="'+memClass+'">'+c[key]+'</td>';
+                } else {
+                    html += '<td>'+c[key]+'</td>';
+                }
+            }
+        }
+        html += '</tr>';
+    }
+    document.getElementById('tbody').innerHTML = html;
+});
---
+++ docker-stats/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {});
+});
+
+io.on('connection', (socket) => {
+    setInterval(() => {
+        exec('docker stats --no-stream', (err, stdout, stderr) => {
+            if (err) {
+                console.error(`exec error: ${err}`);
+                return;
+            }
+
+            let output = stdout.split('\n');
+            let headers = output[0].split(/\s{2,}/);
+            let containers = output.slice(1, -1).map(line => {
+                let data = line.split(/\s{2,}/);
+                let container = {};
+                headers.forEach((header, index) => {
+                    container[header.toLowerCase()] = data[index];
+                });
+
+                // Convert memory usage to MiB and set memUnit accordingly
+                if (container["mem usage / limit"]) {
+                    let mem = container["mem usage / limit"].split('/')[0].trim();
+                    let value = parseFloat(mem);
+                    let suffix = mem.slice(-2);
+
+                    if (suffix == 'Gi') {
+                        value = value * 1024; // convert GiB to MiB
+                        container.memUnit = 1; // GiB
+                    } else { 
+                        container.memUnit = 0; // MiB
+                    }
+
+                    container.memValue = value;
+                }
+
+                return container;
+            });
+
+            socket.emit('docker stats', containers);
+        });
+    }, 2000); // update every 2 seconds
+});
+
+http.listen(3000, () => console.log('Server is running on port 3000'));
