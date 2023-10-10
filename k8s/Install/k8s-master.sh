f0e1ce6b8057afff711d47066dbaf2dd01af1e1c
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-10-10 17:36:46 +0500

    k8s installer

---
+++ k8s/Install/k8s-master.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# Colors for terminal output
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+LIGHT_GREEN='\033[0;32m'
+BROWN='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+LIGHT_GREEN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_LIGHT_GREEN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN} ....."
+cat << "EOF"
+
+$$\   $$\  $$$$$$\   $$$$$$\        $$$$$$\                       $$\               $$\ $$\                     
+$$ | $$  |$$  __$$\ $$  __$$\       \_$$  _|                      $$ |              $$ |$$ |                    
+$$ |$$  / $$ /  $$ |$$ /  \__|        $$ |  $$$$$$$\   $$$$$$$\ $$$$$$\    $$$$$$\  $$ |$$ | $$$$$$\   $$$$$$\  
+$$$$$  /   $$$$$$  |\$$$$$$\          $$ |  $$  __$$\ $$  _____|\_$$  _|   \____$$\ $$ |$$ |$$  __$$\ $$  __$$\ 
+$$  $$<   $$  __$$<  \____$$\         $$ |  $$ |  $$ |\$$$$$$\    $$ |     $$$$$$$ |$$ |$$ |$$$$$$$$ |$$ |  \__|
+$$ |\$$\  $$ /  $$ |$$\   $$ |        $$ |  $$ |  $$ | \____$$\   $$ |$$\ $$  __$$ |$$ |$$ |$$   ____|$$ |      
+$$ | \$$\ \$$$$$$  |\$$$$$$  |      $$$$$$\ $$ |  $$ |$$$$$$$  |  \$$$$  |\$$$$$$$ |$$ |$$ |\$$$$$$$\ $$ |      
+\__|  \__| \______/  \______/       \______|\__|  \__|\_______/    \____/  \_______|\__|\__| \_______|\__|      
+                                                                                                             
+EOF
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+
+# Get the current username
+CURRENT_USERNAME=$(whoami)
+
+# Update and upgrade system packages
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Updating and upgrading system packages...${NC}"
+sudo apt update && sudo apt upgrade -y
+
+# Install required tools
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Installing tools...${NC}"
+apt-get install net-tools jq git htop -y
+
+# Load required modules for containerization
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Loading container modules...${NC}"
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Disable swap (as recommended for K8s)
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Disabling swap...${NC}"
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+
+# Ensure modules are loaded on boot
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+
+# Set sysctl settings for Kubernetes
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# Install additional utilities
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Installing additional utilities...${NC}"
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+
+# Docker installation
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Installing Docker...${NC}"
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Adjust Docker socket permissions
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Adjusting Docker permissions...${NC}"
+sudo chmod 666 /var/run/docker.sock
+sudo chown $CURRENT_USERNAME:docker /var/run/docker.sock
+
+# Configure Docker with overlay2 storage driver and systemd cgroup driver
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN}Configuring Docker...${NC}"
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Restart and enable Docker service
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
+
+# Configure and restart containerd
+echo -e "${LIGHT_GREEN}Configuring containerd...${NC}"
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# Kubernetes installation
+echo -e "${LIGHT_GREEN}Installing Kubernetes (K8s)...${NC}"
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo systemctl enable kubelet
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Initialize Kubernetes cluster
+echo -e "${LIGHT_GREEN}Initializing Kubernetes...${NC}"
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+sudo kubeadm init --pod-network-cidr=10.10.0.0/16 --control-plane-endpoint=k8s-master
+
+# Setup kubectl config
+echo -e "${LIGHT_GREEN}Configuring kubectl...${NC}"
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install Helm 3
+echo -e "${LIGHT_GREEN}Installing Helm 3...${NC}"
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+echo -e "${YELLOW}---------------------------------------------------------------------------------------------------------------------${NC}"
+
+echo -e "${LIGHT_GREEN}Kubernetes has been successfully installed${NC}"
+
+
+
+echo -e "${YELLOW}-------------------------------------------------------------------------------${NC}"
+echo -e "${LIGHT_GREEN} ....."
+cat << "EOF"
+$$\ $$\                                                                             $$\ $$\             
+\$$\\$$\                                                                            \$$\\$$\            
+ \$$\\$$\      $$$$$$$\ $$\   $$\  $$$$$$$\  $$$$$$$\  $$$$$$\   $$$$$$$\  $$$$$$$\  \$$\\$$\           
+  \$$\\$$\    $$  _____|$$ |  $$ |$$  _____|$$  _____|$$  __$$\ $$  _____|$$  _____|  \$$\\$$\          
+  $$  |\$$\   \$$$$$$\  $$ |  $$ |$$ /      $$ /      $$$$$$$$ |\$$$$$$\  \$$$$$$\    $$  |\$$\         
+ $$  /  \$$\   \____$$\ $$ |  $$ |$$ |      $$ |      $$   ____| \____$$\  \____$$\  $$  /  \$$\        
+$$  /    \$$\ $$$$$$$  |\$$$$$$  |\$$$$$$$\ \$$$$$$$\ \$$$$$$$\ $$$$$$$  |$$$$$$$  |$$  /    \$$\       
+\__/      \__|\_______/  \______/  \_______| \_______| \_______|\_______/ \_______/ \__/      \__|      
+                                                                                                        
+EOF
+echo -e "${YELLOW}-------------------------------------------------------------------------------${NC}"
+
+echo -e "${YELLOW}----------------------Next steps-------------------------${NC}"
+echo -e "${YELLOW}[*]Join the worker nodes to the master with kubeadm join${NC}"
+echo -e "${LIGHT_GREEN}---------------------------------------------------------${NC}"
+echo -e "${YELLOW}[*]Install and configure calico on k8s-master${NC}"
+echo -e "${LIGHT_GREEN}---------------------------------------------------------${NC}"
