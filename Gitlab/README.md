589204fee93a2c3fe5526c39a252240feeeb090b
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-10-27 18:02:34 +0500

    bugs fixed

--- Gitlab/README.md
+++ Gitlab/README.md
@@ -1,5 +1,7 @@
 ## Gitlab Installer 
 
 ```bash
-curl -sSL https://raw.githubusercontent.com/ismoilovdevml/devops-tools/master/Gitlab/gitlab-installer.sh | bash
+curl -sSL -o gitlab-installer.sh https://raw.githubusercontent.com/ismoilovdevml/devops-tools/master/Gitlab/gitlab-installer.sh
+chmod +x gitlab-installer.sh
+./gitlab-installer.sh
 ```
--- Gitlab/gitlab-installer.sh
+++ Gitlab/gitlab-installer.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Colors for terminal output
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BROWN='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT_GRAY='\033[0;37m'
-DARK_GRAY='\033[1;30m'
-LIGHT_RED='\033[1;31m'
 LIGHT_GREEN='\033[1;32m'
+GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-LIGHT_BLUE='\033[1;34m'
-LIGHT_PURPLE='\033[1;35m'
-LIGHT_CYAN='\033[1;36m'
-WHITE='\033[1;37m'
-NC='\033[0m' 
+NC='\033[0m' # No Color 
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
