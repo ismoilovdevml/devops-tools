f5a363cf9c91611d092e2ab2828d3cf7eb25c04f
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-11-06 12:30:01 +0500

    updated

--- Bank-Systems/mifos-x/query.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE mifostenant-default;
-CREATE USER 'mifos'@'localhost' IDENTIFIED BY 'ismoilovdev';
-GRANT ALL PRIVILEGES ON mifostenant-default.* TO 'mifos'@'localhost';
-FLUSH PRIVILEGES;
-EXIT;
