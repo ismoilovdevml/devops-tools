99acb3da4b446cd954581396d030114c5e511eec
Otabek Ismoilov <107093598+ismoilovdevml@users.noreply.github.com>
2023-12-04 15:15:22 +0500

    Update installer.sh

--- Docker/installer.sh
+++ Docker/installer.sh
@@ -10,7 +10,7 @@ sudo apt install -y curl gnupg2 software-properties-common apt-transport-https
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -22,7 +22,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
 # Adjust Docker socket permissions
 sudo chmod 666 /var/run/docker.sock
